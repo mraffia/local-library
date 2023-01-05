8725df28a7e7d94b988fea52913caeab9bbb6d6a
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-01-05 18:11:40 +0700

    Replace skeleton controller for all books page with actual logic to find all Book objects and returning title and author to the view

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -36,7 +36,16 @@ exports.index = (req, res) => {
 
 // Display list of all books.
 exports.book_list = (req, res) => {
-  res.send("NOT IMPLEMENTED: Book list");
+  Book.find({}, "title author")
+    .sort({ title: 1 })
+    .populate("author")
+    .exec(function (err, list_books) {
+      if (err) {
+        return next(err);
+      }
+      //Successful, so render
+      res.render("book_list", { title: "Book List", book_list: list_books });
+    });
 };
 
 // Display detail page for a specific book.
