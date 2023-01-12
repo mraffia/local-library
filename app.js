c54ac8a6b3b82a7a7ec38433fc4640cbd8283cb3
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-01-13 03:27:09 +0700

    Modify the LocalLibrary website to get the database URI from the OS environment (if it has been defined), and otherwise use our development database

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ app.use(helmet());
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
-const mongoDB = "mongodb+srv://mraffia:poopski@cluster0.etacmtw.mongodb.net/local_library?retryWrites=true&w=majority";
+const dev_db_url = "mongodb+srv://mraffia:poopski@cluster0.etacmtw.mongodb.net/local_library?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
