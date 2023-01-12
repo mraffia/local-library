8b2dd04f0ce35feb8e14e88a2617008ed3166625
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-01-12 19:27:05 +0700

    Replace author_update_get skeleton controller to logic that renders update author page, also replace author_update_post to logic that process the user input and updates the data in db

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -86,17 +86,6 @@ exports.author_create_post = [
     // Extract the validation errors from a request.
     const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-      // There are errors. Render form again with sanitized values/errors messages.
-      res.render("author_form", {
-        title: "Create Author",
-        author: req.body,
-        errors: errors.array(),
-      });
-      return;
-    }
-    // Data from form is valid.
-
     // Create an Author object with escaped and trimmed data.
     const author = new Author({
       first_name: req.body.first_name,
@@ -104,6 +93,18 @@ exports.author_create_post = [
       date_of_birth: req.body.date_of_birth,
       date_of_death: req.body.date_of_death,
     });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/errors messages.
+      res.render("author_form", {
+        title: "Create Author",
+        author,
+        errors: errors.array(),
+      });
+      return;
+    }
+
+    // Data from form is valid.
     author.save((err) => {
       if (err) {
         return next(err);
@@ -181,11 +182,84 @@ exports.author_delete_post = (req, res, next) => {
 };
 
 // Display Author update form on GET.
-exports.author_update_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author update GET");
+exports.author_update_get = (req, res, next) => {
+  Author.findById(req.params.id)
+    .exec((err, author) => {
+      if (err) {
+        return next(err);
+      }
+      if (author == null) {
+        // No results.
+        const err = new Error("Author not found");
+        err.status = 404;
+        return next(err);
+      }
+      // Successful, so render.
+      res.render("author_form", {
+        title: 'Update Author',
+        author,
+      });
+    });
 };
 
 // Handle Author update on POST.
-exports.author_update_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author update POST");
-};
+exports.author_update_post = [
+  // Validate and sanitize fields.
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified.")
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters."),
+  body("family_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Family name must be specified.")
+    .isAlphanumeric()
+    .withMessage("Family name has non-alphanumeric characters."),
+  body("date_of_birth", "Invalid date of birth")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  body("date_of_death", "Invalid date of death")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create an Author object with escaped and trimmed data.
+    const author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/errors messages.
+      res.render("author_form", {
+        title: "Update Author",
+        author,
+        errors: errors.array(),
+      });
+      return;
+    }
+
+    // Data from form is valid. Update the record.
+    Author.findByIdAndUpdate(req.params.id, author, {}, (err, theauthor) => {
+      if (err) {
+        return next(err);
+      }
+
+      // Successful: redirect to author detail page.
+      res.redirect(theauthor.url);
+    });
+  },
+];
