3c8345200d7e77382993316ba629c7cf28d67c31
Mohammad Raffi Akbar <mohraffia@gmail.com>
2023-01-12 19:28:48 +0700

    Add another date of birth and date of death virtual propertie to Author model for date input field on the form that requires YYYY-MM-DD format

--- models/author.js
+++ models/author.js
@@ -42,5 +42,13 @@ AuthorSchema.virtual("lifespan").get(function () {
   return (this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '') + ' - ' + (this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '');
 });
 
+AuthorSchema.virtual("date_of_birth_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.date_of_birth).toISODate();
+});
+
+AuthorSchema.virtual("date_of_death_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.date_of_death).toISODate();
+});
+
 // Export model
 module.exports = mongoose.model("Author", AuthorSchema);
